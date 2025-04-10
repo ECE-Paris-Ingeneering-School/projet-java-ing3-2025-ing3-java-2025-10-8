7938ce8281096ebe9111a2c81f3bd4aad692fe6e
test <clara.leal@edu.ece.fr>
2025-04-10 12:15:43 +0200

    OffreReduction.java

---
+++ src/model/OffreReduction.java
@@ -0,0 +1,95 @@
+package model;
+
+import java.sql.Date;
+
+public class OffreReduction {
+
+    private int idOffre;
+    private int idUtilisateur;
+    private String description;
+    private double pourcentageReduction;
+    private Date dateDebut;
+    private Date dateFin;
+
+    // Constructeurs
+    public OffreReduction() {}
+
+    public OffreReduction(int idUtilisateur, String description, double pourcentageReduction, Date dateDebut, Date dateFin) {
+        this.idUtilisateur = idUtilisateur;
+        this.description = description;
+        this.pourcentageReduction = pourcentageReduction;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+    }
+
+    public OffreReduction(int idOffre, int idUtilisateur, String description, double pourcentageReduction, Date dateDebut, Date dateFin) {
+        this.idOffre = idOffre;
+        this.idUtilisateur = idUtilisateur;
+        this.description = description;
+        this.pourcentageReduction = pourcentageReduction;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+    }
+
+    // Getters et Setters
+    public int getIdOffre() {
+        return idOffre;
+    }
+
+    public void setIdOffre(int idOffre) {
+        this.idOffre = idOffre;
+    }
+
+    public int getIdUtilisateur() {
+        return idUtilisateur;
+    }
+
+    public void setIdUtilisateur(int idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPourcentageReduction() {
+        return pourcentageReduction;
+    }
+
+    public void setPourcentageReduction(double pourcentageReduction) {
+        this.pourcentageReduction = pourcentageReduction;
+    }
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    // ToString
+    @Override
+    public String toString() {
+        return "OffreReduction{" +
+                "idOffre=" + idOffre +
+                ", idUtilisateur=" + idUtilisateur +
+                ", description='" + description + '\'' +
+                ", pourcentageReduction=" + pourcentageReduction +
+                ", dateDebut=" + dateDebut +
+                ", dateFin=" + dateFin +
+                '}';
+    }
+}
