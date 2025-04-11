287261603644929cc3bc0a9963b4cfca8762a259
Marion BRAIDA <braidamarion@gmail.com>
2025-04-11 10:13:33 +0200

    implément connexionBdd

--- src/DAO/ConnexionBdd.java
+++ src/DAO/ConnexionBdd.java
@@ -1,27 +1,23 @@
-package DAO;
+package connexionBdd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnexionBdd {
-
-    private static final String URL = "jdbc:mysql://localhost:3306/booking?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/bookingjava";//connexion a la bdd
     private static final String UTILISATEUR = "root";
     private static final String MOT_DE_PASSE = "";
 
+    //methode connexion à la bdd
     public static Connection seConnecter() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
             Connection connexion = DriverManager.getConnection(URL, UTILISATEUR, MOT_DE_PASSE);
             System.out.println("Connexion OKAY");
             return connexion;
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver MySQL introuvable : " + e.getMessage());
         } catch (SQLException e) {
             System.out.println("Erreur de connexion : " + e.getMessage());
+            return null;
         }
-        return null;
     }
 }
