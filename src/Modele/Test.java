9721d399f52dc951e765f5bd09209bb0dcf84d1c
test <clara.leal@edu.ece.fr>
2025-04-26 20:05:46 +0200

    Commentaire

--- src/Modele/Test.java
+++ src/Modele/Test.java
@@ -1,6 +1,15 @@
 package Modele;
 
+/**
+ * Classe de test pour démontrer la création d'un utilisateur et l'affichage de son nom.
+ */
+
 public class Test {
+    /**
+     * Méthode principale pour exécuter le test.
+     *
+     * @param args Arguments de la ligne de commande.
+     */
     public static void main(String[] args) {
         Utilisateur u = new Utilisateur(1, "John", "Doe", "john@mail.com", "1234");
         System.out.println("Nom : " + u.nom);
