61e8e386f3f81a02924e2330e95202d6d626e52f
test <clara.leal@edu.ece.fr>
2025-04-26 19:55:17 +0200

    Commentaire

--- src/DAO/ConnexionBdd.java
+++ src/DAO/ConnexionBdd.java
@@ -4,12 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Classe qui gère la connexion à la base de données.
+ */
 public class ConnexionBdd {
-    private static final String URL = "jdbc:mysql://localhost:3306/bookingjava";//connexion a la bdd
+    private static final String URL = "jdbc:mysql://localhost:3306/bookingjava"; // Connexion a la bdd
     private static final String UTILISATEUR = "root";
     private static final String MOT_DE_PASSE = "";
 
-    //methode connexion à la bdd
+    /**
+     * Établit une connexion à la base de données.
+     *
+     * @return Un objet Connection si la connexion est ok, sinon null.
+     */
     public static Connection seConnecter() {
         try {
             Connection connexion = DriverManager.getConnection(URL, UTILISATEUR, MOT_DE_PASSE);
